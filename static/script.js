58c7157693378f288829ff34f15b343833a285bf
natruszka <ola.michalska2002@gmail.com>
2025-12-02 22:04:10 +0100

    Add front gimmicks

--- static/script.js
+++ static/script.js
@@ -2,7 +2,8 @@ const gestures = ['hand start', 'first digit touch', 'both start load phase', 'l
 
 async function randomizeAndPredictAsync() 
 {
-    signal = [Array.from({length: 32}).map(() => Array.from({length: 1024}).map(() =>Math.random() * 32000 - 8500))]
+    signal = [Array.from({length: 32}).map(() => Array.from({length: 1024}).map(() => Math.random() * 32000 - 8500))]
+    await displayRandomNumbers();
     const response = await fetch("/predict",{
             method: 'POST',
             headers: {
@@ -21,6 +22,25 @@ async function randomizeAndPredictAsync()
 
         const gesturePlaceholder = document.getElementById('gesture')
         gesturePlaceholder.innerHTML = gestures.length > 0 ? gestures.join(',') : 'no gesture'
+}
+
+async function displayRandomNumbers() {
+    const elements = document.querySelectorAll('.eeg');
+
+    elements.forEach(el => {
+    let count = 0;
+    const maxFlashes = 5;
+    const intervalSpeed = 60;
+
+    const interval = setInterval(() => {
+        el.textContent = (Math.random() * 32000 - 8500).toFixed(0);
+        count++;
+
+        if (count >= maxFlashes) {
+            clearInterval(interval);
+        }
+    }, intervalSpeed);
+    });
 }
 
 function signalToGesture(model_output)
